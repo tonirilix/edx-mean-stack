f68241035b10375a3dc77dbd06aaf4a18b151ad2
tonirilix <jesusmontes.mx@hotmail.com>
2016-04-30 20:27:41 -0500

    Do Homework\!

--- 01/homework/interface.js
+++ 01/homework/interface.js
@@ -2,8 +2,10 @@
  *  Inserts "doc" into the collection "movies".
  */
 exports.insert = function(db, doc, callback) {
-  // TODO: implement
-  callback(null);
+	// TODO: implement
+	db.collection('movies').insert(doc, function(error, result) {
+		callback(error);
+	});
 };
 
 /*
@@ -13,6 +15,11 @@ exports.insert = function(db, doc, callback) {
  *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
  */
 exports.byDirector = function(db, director, callback) {
-  // TODO: implement
-  callback(null, []);
+	// TODO: implement
+
+	db.collection('movies').find({
+		'director': director
+	}).sort({'title': 1}).toArray(function(error, docs) {
+		callback(error, docs);
+	});	
 };
