3d16dbfade53e43cf0491cfc6bbd429c634e6808
tonirilix <jesusmontes.mx@hotmail.com>
2016-04-30 19:24:49 -0500

    Require basics

---
+++ 01/index3.js
@@ -0,0 +1,10 @@
+var mongodb = require('mongodb');
+
+var uri = 'mongodb://localhost:27017/example';
+mongodb.MongoClient.connect(uri, function(error, db) {
+	if (error) {
+		console.log(error);
+		process.exit(1);
+	}
+	db.collection('movies').find().toArray(console.log);
+});
